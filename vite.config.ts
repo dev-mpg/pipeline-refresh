dd50427b9f86de96cab197595b8417bbc6673bdf
dev-mpg <dev@mpg-plc.com>
2026-02-12 17:21:10 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // [https://vitejs.dev/config/](https://vitejs.dev/config/)
 export default defineConfig(({ mode }) => ({
+  base: '/pipeline-refresh/',
   server: {
     host: "::",
     port: 8080,
